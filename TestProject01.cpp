dea53e8bb912cabb0ed3858c60cdef9e7a3a333b
amit-kumar1 <amit.kumar1@rolta.com>
2016-01-27 11:27:36 +0530

    Create TestProject01.cpp

---
+++ TestProject01.cpp
@@ -0,0 +1,16 @@
+// TestProject01.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"test project 01"<<endl;
+	int i;
+	cin>>i;
+	return 0;
+}
+
